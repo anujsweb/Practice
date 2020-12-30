a695c1f27c52b049f24a863505733cd130bbf32c
anujsweb <55160147+anujsweb@users.noreply.github.com>
2020-12-31 00:02:48 +0530

    feat: banana translator added

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-function onClickHandler(userInput) {
-console.log(userInput)
-}
+var txtInput = document.querySelector("#txt-input");
+var inpButton = document.querySelector("#button1");
+var outputDiv = document.querySelector("#output");
+
+serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function Handler() {
+    Inp = txtInput.value;
+    textInp = serverUrl + "?text=" + Inp;
+
+    fetch(textInp).then(response => response.json()).then(json => {
+        var getTranslation = json.contents.translated;
+        outputDiv.innerText = getTranslation;
+    })
+};
+
+inpButton.addEventListener("click", Handler);
